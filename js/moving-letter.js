8d0bce637183c78a9a84eb2d607685268bd22a21
Boomer <clevelandwilliams3579@gmail.com>
2019-04-04 21:43:49 -0400

    updated template

--- js/moving-letter.js
+++ js/moving-letter.js
@@ -19,5 +19,29 @@ anime.timeline({ loop: true })
         opacity: 0,
         duration: 1000,
         easing: "easeOutExpo",
-        delay: 30000
+        delay: 1000
+    });
+
+// Wrap every letter in a span
+$('.ml3').each(function () {
+    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({
+        loop: true
+    })
+    .add({
+        targets: '.ml3 .letter',
+        opacity: [0, 1],
+        easing: "easeInOutQuad",
+        duration: 2250,
+        delay: function (el, i) {
+            return 150 * (i + 1)
+        }
+    }).add({
+        targets: '.ml3',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
     });
